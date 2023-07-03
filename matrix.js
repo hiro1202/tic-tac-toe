fe134c6d616d7600b5ad011c14ae1b678aa41cf6
Keiichiro Bando <keiichirobando21@gmail.com>
2023-07-03 14:30:57 -0600

    Developed feature for selecting squares and added new HTML file.

--- matrix.js
+++ matrix.js
@@ -31,8 +31,9 @@ class Matrix{
         this.matrix[x][y] = mark;
     }
 }
-    
-    //init matrix
+
+
+//init matrix
 class Matrix_funcs{
 
     /*
@@ -141,3 +142,77 @@ result = funcs.getIsGameOverAndWinner(matrix.matrix, matrix.sideMatrix)
 console.log(result)
 console.log(matrix)
 */
+
+
+//MatrixとMatrix_funcsのインスタンスを生成
+let matrix = new Matrix();
+const funcs = new Matrix_funcs();
+
+//先手として◯を設定
+let turn = '〇';
+
+/*
+現在のボードの状態を表示し、各セルにクリックイベントを追加
+クリックイベントは、セルがクリックされた時に発火し、
+選択されたセルにマークを付け、ゲームの状態をチェック
+ゲームが終了している場合、アラートを表示して、ゲームを再初期化
+*/
+
+function renderBoard() {
+    //HTML内のidの'board'の要素(div要素）を取得し、その中身を空にする
+    const board = document.getElementById('board');
+    //board要素の中身を空にする
+    board.innerHTML = '';
+    //ボード（matrix）の各行に対してループ。
+    for(let i = 0; i < matrix.sideMatrix; i++){
+        //各行に対するdiv要素（行）を新たに作成
+        let row = document.createElement('div');
+        //div要素（行）に、'rowと'いうクラス名を設定
+        row.classList.add('row');
+        //各行内の各セルに対してさらにループ。
+               //このセルにはテキスト（〇かX）を設定し、'cell'というクラスを追加
+        for(let j = 0; j < matrix.sideMatrix; j++){
+            //各セルに対するループ内で新たにdiv要素（セル）を作成
+            let cell = document.createElement('div');
+            //cellのテキストにmatrixの(i, j)要素の値をセットする。値がなければ0をセット
+            cell.innerText = matrix.matrix[i][j] || ' ';
+            // cellに対して'cell'というクラスを追加する
+            cell.classList.add('cell');
+            //各セルに'click'イベントリスナーを追加
+            cell.addEventListener('click', function() {
+                //すでにマークがついている場合(0でない場合)は何もしない(return)
+                if(matrix.matrix[i][j] !== 0) return;
+                //選択されたセルに、現在のプレイヤーのマークをつける
+                matrix.markMatrixElement(i, j, turn);
+                //現在のプレーヤーを切り替える
+                turn = turn === '〇' ? 'X' : '〇';
+                // ボードを再描画して、現在のゲームの状態を反映
+                renderBoard();
+                //ゲームが終了したか判定し結果を、変数resultに代入
+                let result = funcs.getIsGameOverAndWinner(matrix.matrix, matrix.sideMatrix);
+                //result.isGameOver が true の場合、
+                if(result.isGameOver) {
+                    //result.winner === 'draw'のとき「引き分け！」を表示
+                    //そうでない場合 '〇の勝利!' または 'Xの勝利!'を表示
+                    alert(result.winner === 'draw' ? '引き分け!' : `${result.winner}の勝利!`);
+                    //マトリクスを初期化
+                    matrix.initialize();
+                    //ボードを再描画
+                    renderBoard();
+                }
+            });
+            //rowにcellを追加する
+            row.appendChild(cell);
+        }
+        //boardにrowを追加する
+        board.appendChild(row);
+    }
+}
+
+//ゲーム途中で、ボードを再初期化するリセット機能
+document.getElementById('reset').addEventListener('click', function() {
+    matrix.initialize();
+    renderBoard();
+});
+
+renderBoard();
